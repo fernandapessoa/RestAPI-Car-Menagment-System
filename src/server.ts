9343745285daf7f566635eb642716b675000b11e
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-04 17:08:51 -0400

    feat: start server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { connect } from './shared/infra/database/db';
+
+import { app } from './app';
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, async () => {
+	await connect();
+	console.log(
+		`Listening to port ${port}, environment: ${process.env.NODE_ENV}`
+	);
+});
