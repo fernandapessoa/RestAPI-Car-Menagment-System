cebc1f92d6654569b00a181f316534332cf96535
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-05 02:46:19 -0400

    feat: add user type

---
+++ src/types/userType.ts
@@ -0,0 +1,16 @@
+interface User {
+	_id: string;
+	name: string;
+	birth: Date;
+	email: string;
+	password: string;
+	cep: string;
+	qualified: string;
+    patio: string;
+	complement: string;
+    neighborhood: string;
+	locality: string;
+	
+}
+
+export { User };
