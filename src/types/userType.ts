61332ca7028c4b6e3b64d350de735307e304f185
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-06 02:50:36 -0400

    refactor: update userTypes interface attributes

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,16 +1,17 @@
 interface User {
 	_id: string;
 	name: string;
+	cpf: string;
 	birth: Date;
 	email: string;
 	password: string;
 	cep: string;
 	qualified: string;
-    patio: string;
+	patio: string;
 	complement: string;
-    neighborhood: string;
+	neighborhood: string;
 	locality: string;
-	
+	uf: string;
 }
 
 export { User };
