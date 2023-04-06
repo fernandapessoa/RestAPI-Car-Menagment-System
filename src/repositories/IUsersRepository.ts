474607aa555c0f5aafccafb503959c4f4256ca22
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-06 02:34:14 -0400

    refactor: remove unused interface repository directories

--- src/repositories/IUsersRepository.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { User } from '../types/userType';
-
-interface IUsersRepository {
-	registerUser(user: User): Promise<User | null>;
-}
-
-export { IUsersRepository };
