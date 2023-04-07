d3f10448aee88012f89049cbca1caf9624104972
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-07 19:58:51 -0400

    feat: add auth to authenticated routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { Request, Response, NextFunction } from 'express';
+import { auth } from '../middlewares/auth';
 import { userController } from '../modules/user/userController';
 //import { userController } from '../modules/user/userController';
 
@@ -8,13 +8,13 @@ const usersRouter = Router();
 usersRouter.post('/', (req, res, next) =>
 	userController.registerUser(req, res, next)
 );
-usersRouter.get('/:id', (req, res, next) =>
+usersRouter.get('/', auth, (req, res, next) =>
 	userController.getUser(req, res, next)
 );
-usersRouter.patch('/:id', (req, res, next) =>
+usersRouter.patch('/', auth, (req, res, next) =>
 	userController.updateUser(req, res, next)
 );
-usersRouter.delete('/:id', (req, res, next) =>
+usersRouter.delete('/', auth, (req, res, next) =>
 	userController.deleteUser(req, res, next)
 );
 
