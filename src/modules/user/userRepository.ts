cefed08ad33f384af6b06f4e6b27eb0479c39bbc
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-07 01:19:10 -0400

    feat: add getUser to userRepository

---
+++ src/modules/user/userRepository.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { User } from './IUser';
+import { IUserRepository } from './IUserRepository';
+import { userSchema } from './userSchema';
+//import { UpdateQuery } from 'mongoose';
+
+export class MongoUserRepository implements IUserRepository {
+	async registerUser(user: User): Promise<User | null> {
+		const registeredUser = await userSchema.create(user);
+		return registeredUser;
+	}
+
+	async getUser(userId: string): Promise<User | null> {
+		console.log('chegou aqui');
+		console.log(userId);
+
+		const user = await userSchema.findById(userId);
+		return user;
+	}
+
+	async deleteUser(userId: string): Promise<User | null> {
+		return null;
+	}
+	async updateUser(userId: string): Promise<User | null> {
+		return null;
+	}
+	async findUserByEmail(email: string): Promise<User | null> {
+		return null;
+	}
+}
+
+const userRepository: IUserRepository = new MongoUserRepository();
+export { userRepository };
