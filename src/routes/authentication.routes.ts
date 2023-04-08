94207274879d25d10daa583f3cc33ad6fbe7ee22
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-07 21:42:49 -0400

    feat: add authenticateUser to authenticate route

--- src/routes/authentication.routes.ts
+++ src/routes/authentication.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { userController } from '../modules/user/userController';
 
 const authenticationRouter = Router();
 
-authenticationRouter.post('/');
+authenticationRouter.post('/', (req, res, next) =>
+	userController.authenticateUser(req, res, next)
+);
 
 export { authenticationRouter };
