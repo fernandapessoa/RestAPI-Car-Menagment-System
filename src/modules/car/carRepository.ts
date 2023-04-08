0cdb0d80f9adb6042d4341f15dc80cad01e652a9
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 01:42:46 -0400

    feat: add registerCar to MongoCarRepository

---
+++ src/modules/car/carRepository.ts
@@ -0,0 +1,19 @@
+import { Car } from './ICar';
+import { ICarRepository } from './ICarRepository';
+import { carSchema } from './carSchema';
+import { UpdateQuery } from 'mongoose';
+
+export class MongoCarRepository implements ICarRepository {
+	async registerCar(car: Car): Promise<Car | null> {
+		const registeredCar = await carSchema.create(car);
+		return registeredCar;
+	}
+
+	async getAllCars(): Promise<Car[]> {}
+	async getCarById(carId: string): Promise<Car | null> {}
+	async getCarByAccessoryId(accessoriesId: string): Promise<Car[]> {}
+	async deleteCarById(carId: string): Promise<Car | null> {}
+	async updateCar(carId: string, updateParams: unknown): Promise<Car | null> {}
+}
+
+export { ICarRepository };
