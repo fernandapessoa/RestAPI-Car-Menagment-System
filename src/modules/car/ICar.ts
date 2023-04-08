339c5f9c4515c573d41ddd2db139daeae88f0b60
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 02:27:45 -0400

    feat: update ICar adding interface Accessory

--- src/modules/car/ICar.ts
+++ src/modules/car/ICar.ts
@@ -1,12 +1,15 @@
+interface Accessory {
+	_id: string;
+	description: string;
+}
+
 interface Car {
 	_id: string;
 	model: string;
 	color: string;
 	year: number;
 	value_per_day: number;
-	accessories: {
-		description: string;
-	}[];
+	accessories: Accessory[];
 	number_of_passengers: number;
 }
 
