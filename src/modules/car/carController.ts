11a1cc450d104ea35202d983871c658532ce1c4e
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 05:54:10 -0400

    feat; add registerCar and gatAllCars to CarController

---
+++ src/modules/car/carController.ts
@@ -0,0 +1,35 @@
+import { CarService, carService } from './carService';
+import { Request, Response, NextFunction } from 'express';
+import { Car } from './ICar';
+import { CatchExpressError } from '../../decorators/CatchExpressError';
+
+export class CarController {
+	private carService: CarService;
+
+	constructor(carService: CarService) {
+		this.carService = carService;
+	}
+
+	@CatchExpressError
+	async registerCar(req: Request, res: Response, _next: NextFunction) {
+		const carData: Car = req.body;
+		const car = await this.carService.registerCar(carData);
+
+		return res.status(201).json({
+			status: 'success',
+			data: car,
+		});
+	}
+
+	async gatAllCars(req: Request, res: Response, _next: NextFunction) {
+		const cars = await this.carService.getAllCars();
+		return res.status(200).json({
+			status: 'success',
+			data: cars,
+			total: cars.length,
+		});
+	}
+}
+
+const carController = new CarController(carService);
+export { carController };
