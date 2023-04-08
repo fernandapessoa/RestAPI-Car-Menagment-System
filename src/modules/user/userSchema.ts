9c2cdd313c48525638660c5e7e9bf553e3b52443
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 01:45:06 -0400

    feat: update userSchama

--- src/modules/user/userSchema.ts
+++ src/modules/user/userSchema.ts
@@ -11,6 +11,8 @@ const UserSchema = new Schema<User>({
 	cpf: {
 		type: String,
 		required: [true, 'A valid CPF must be provided'],
+		unique: true,
+		trim: true,
 	},
 	birth: {
 		type: Date,
@@ -18,45 +20,51 @@ const UserSchema = new Schema<User>({
 		min: new Date(new Date().setFullYear(new Date().getFullYear() - 120)),
 		max: new Date(new Date().setFullYear(new Date().getFullYear() - 18)),
 	},
-
 	email: {
 		type: String,
-		required: [true, 'A email must be provided'],
-		//unique: true,
+		required: [true, 'An email must be provided'],
+		unique: true,
 		lowercase: true,
+		trim: true,
 	},
-
 	password: {
 		type: String,
 		minlength: 8,
 		required: [true, 'A password must be provided'],
 	},
-
 	cep: {
 		type: String,
-		lengh: 8,
+		length: 8,
 		required: [true, 'A CEP must be provided'],
+		trim: true,
 	},
 	qualified: {
 		type: String,
 		required: [true, 'A qualified status must be provided'],
+		trim: true,
 	},
 	patio: {
 		type: String,
+		trim: true,
 	},
 	complement: {
 		type: String,
+		trim: true,
 	},
 	neighborhood: {
 		type: String,
+		trim: true,
 	},
 	locality: {
 		type: String,
+		trim: true,
 	},
 	uf: {
 		type: String,
+		trim: true,
 	},
 });
 
-const userSchema = model('User', UserSchema);
+const userSchema = model<User>('User', UserSchema);
+
 export { userSchema };
