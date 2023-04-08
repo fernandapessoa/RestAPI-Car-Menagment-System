e87a7f87489aa076847513dbe0dc27f0a544cc61
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 02:26:12 -0400

    feat: add getCarByAccessoryId and getCarByQueryParam

--- src/modules/car/carRepository.ts
+++ src/modules/car/carRepository.ts
@@ -18,8 +18,14 @@ export class MongoCarRepository implements ICarRepository {
 		const car = await carSchema.findOne({ _id: carId });
 		return car;
 	}
-	async getCarByAccessoryId(accessoriesId: string): Promise<Car[]> {}
-	getCarByParam(queryParam: string): Promise<Car[]> {}
+	async getCarByAccessoryId(accessoryId: string): Promise<Car[]> {
+		const car = await carSchema.find({ 'accessories._id': accessoryId });
+		return car;
+	}
+	async getCarByQueryParam(attribute: string, value: string): Promise<Car[]> {
+		const cars = await carSchema.find({ attribute: value });
+		return cars;
+	}
 	async deleteCarById(carId: string): Promise<Car | null> {}
 	async updateCar(carId: string, updateParams: unknown): Promise<Car | null> {}
 }
