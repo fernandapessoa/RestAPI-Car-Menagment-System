5a85bb3ecc5a28ebab5e15fc55027ef98d5998c5
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 18:12:28 -0400

    feat: add joi validator to user data

--- src/validators/userValidators.ts
+++ src/validators/userValidators.ts
@@ -1,30 +1,58 @@
-// import Joi from 'joi';
+/* eslint-disable no-useless-escape */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Joi from 'joi';
+import joiDate from '@joi/date';
+import { isValidCpf } from '../utils/isValidCpf';
+const joiExtended = Joi.extend(joiDate);
 
-// const createUserSchema = Joi.object({
-// 	name: Joi.string().min(6).max(65).required(),
-// 	//validar cpf ainda
-// 	cpf: Joi.string()
-// 		.pattern(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
-// 		.required(),
-// 	birth: Joi.date()
-// 		.required()
-// 		.max('now')
-// 		.iso()
-// 		.raw()
-// 		.custom((value, helper) => {
-// 			const idade = new Date().getFullYear() - new Date(value).getFullYear();
-// 			if (idade < 18 || idade > 120) {
-// 				return helper.message(
-// 					'The user must be over 18 years old and under 120.'
-// 				);
-// 			}
-// 			return value;
-// 		}),
-// 	email: Joi.string().email().required(),
-// 	password: Joi.string().min(8).required(),
-// 	//usar a api
-// 	cep: Joi.string()
-// 		.pattern(/^\d{5}\-\d{3}$/)
-// 		.required(),
-// 	qualified: Joi.boolean().required(),
-// });
+const createUserSchema = Joi.object({
+	name: Joi.string().min(3).max(65).required(),
+	birth: joiExtended
+		.date()
+		.format('DD/MM/YYYY')
+		.min(new Date(new Date().getFullYear() - 130))
+		.max(new Date(Date.now() - 18))
+		.required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).required(),
+	cpf: Joi.string()
+		.pattern(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+		.custom((value: string, helper: any) => {
+			if (!isValidCpf(value)) {
+				return helper.message('Invalid CPF');
+			}
+			return value;
+		})
+		.required(),
+	cep: Joi.string()
+		.pattern(/^\d{8}$/)
+		.required(),
+	qualified: Joi.string().valid('sim', 'não').required(),
+});
+
+const updateUserSchema = Joi.object({
+	name: Joi.string().min(3).max(65).optional(),
+	birth: joiExtended
+		.date()
+		.format('DD/MM/YYYY')
+		.min(new Date(new Date().getFullYear() - 130))
+		.max(new Date(Date.now() - 18))
+		.optional(),
+	email: Joi.string().email().optional(),
+	password: Joi.string().min(6).optional(),
+	cpf: Joi.string()
+		.pattern(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+		.custom((value: string, helper: any) => {
+			if (!isValidCpf(value)) {
+				return helper.message('Invalid CPF');
+			}
+			return value;
+		})
+		.optional(),
+	cep: Joi.string()
+		.pattern(/^\d{8}$/)
+		.optional(),
+	qualified: Joi.string().valid('sim', 'não').optional(),
+}).unknown(true);
+
+export { createUserSchema, updateUserSchema };
