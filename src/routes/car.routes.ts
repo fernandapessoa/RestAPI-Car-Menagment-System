307bba1661feeee0ec0afe2eeb5880b324318710
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 19:40:45 -0400

    refactor: update routes without callback function

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,21 +1,14 @@
 import { Router } from 'express';
 import { carController } from '../modules/car/carController';
+import { validateCarData } from '../middlewares/validateCarData';
 
 const carsRouter = Router();
 
-carsRouter.post('/', (req, res, next) =>
-	carController.registerCar(req, res, next)
-);
-carsRouter.get('/', (req, res, next) => carController.gatCars(req, res, next)); //get all or by query param
-carsRouter.get('/:id', (req, res, next) =>
-	carController.getCarById(req, res, next)
-);
-carsRouter.delete('/:id', (req, res, next) =>
-	carController.deleteCarById(req, res, next)
-);
-carsRouter.put('/:id', (req, res, next) =>
-	carController.updateCarById(req, res, next)
-);
+carsRouter.post('/', validateCarData, carController.registerCar);
+carsRouter.get('/', carController.gatCars); //get all or by query param
+carsRouter.get('/:id', carController.getCarById);
+carsRouter.delete('/:id', carController.deleteCarById);
+carsRouter.put('/:id', carController.updateCarById);
 carsRouter.patch('/:carId/accessories/:accerryId');
 
 export { carsRouter };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -9,17 +9,9 @@ import {
 
 const usersRouter = Router();
 
-usersRouter.post('/', validateUserData, (req, res, next) =>
-	userController.registerUser(req, res, next)
-);
-usersRouter.get('/', auth, (req, res, next) =>
-	userController.getUser(req, res, next)
-);
-usersRouter.patch('/', auth, validateUpdateUserData, (req, res, next) =>
-	userController.updateUser(req, res, next)
-);
-usersRouter.delete('/', auth, (req, res, next) =>
-	userController.deleteUser(req, res, next)
-);
+usersRouter.post('/', validateUserData, userController.registerUser);
+usersRouter.get('/', auth, userController.getUser);
+usersRouter.patch('/', auth, validateUpdateUserData, userController.updateUser);
+usersRouter.delete('/', auth, userController.deleteUser);
 
 export { usersRouter };
