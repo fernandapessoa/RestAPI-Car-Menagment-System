f96a241ef27bfa2acd217c281fa91e933203314e
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 18:11:07 -0400

    feat: add user validation middleware to user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
 import { auth } from '../middlewares/auth';
 import { userController } from '../modules/user/userController';
+import {
+	validateUserData,
+	validateUpdateUserData,
+} from '../middlewares/validateUserData';
 //import { userController } from '../modules/user/userController';
 
 const usersRouter = Router();
 
-usersRouter.post('/', (req, res, next) =>
+usersRouter.post('/', validateUserData, (req, res, next) =>
 	userController.registerUser(req, res, next)
 );
 usersRouter.get('/', auth, (req, res, next) =>
 	userController.getUser(req, res, next)
 );
-usersRouter.patch('/', auth, (req, res, next) =>
+usersRouter.patch('/', auth, validateUpdateUserData, (req, res, next) =>
 	userController.updateUser(req, res, next)
 );
 usersRouter.delete('/', auth, (req, res, next) =>
