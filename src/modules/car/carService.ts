a7ff8e87859599a6690628062435c50483ef91bb
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 05:55:57 -0400

    feat: add registerCar and getAllCars to CarService

---
+++ src/modules/car/carService.ts
@@ -0,0 +1,28 @@
+import { ICarRepository } from './ICarRepository';
+import { carRepository } from './carRepository';
+import { Car } from './ICar';
+import { AppError } from '../../errors/AppError';
+
+export class CarService {
+	private carRepository: ICarRepository;
+
+	constructor(carRepository: ICarRepository) {
+		this.carRepository = carRepository;
+	}
+
+	async registerCar(carData: Car) {
+		const registerCar = await this.carRepository.registerCar(carData);
+		if (!registerCar) {
+			throw new AppError(400, 'Car already exists');
+		}
+		return registerCar;
+	}
+
+	async getAllCars() {
+		const cars = await this.carRepository.getAllCars();
+		return cars;
+	}
+}
+
+const carService = new CarService(carRepository);
+export { carService };
