7873d92efd86089e648543db8b55dce0ba472aa0
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 06:02:08 -0400

    feat: add carController functions to car routes

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import { carController } from '../modules/car/carController';
 
 const carsRouter = Router();
 
-carsRouter.post('/'); //register
+carsRouter.post('/', (req, res, next) =>
+	carController.registerCar(req, res, next)
+);
+carsRouter.get('/', (req, res, next) =>
+	carController.gatAllCars(req, res, next)
+); //get all ou por query param
+carsRouter.get('/:id', (req, res, next) =>
+	carController.getCarById(req, res, next)
+);
+carsRouter.delete('/:id');
+carsRouter.put('/:id');
 
 export { carsRouter };
