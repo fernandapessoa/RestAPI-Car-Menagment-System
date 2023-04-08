0bd97c739b6e07e75310d1c1ccb132a9bb86f797
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 01:43:13 -0400

    feat: add CarSchema

---
+++ src/modules/car/carSchema.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from 'mongoose';
+import { Car } from './ICar';
+
+const CarSchema = new Schema<Car>(
+	{
+		model: {
+			type: String,
+			required: [true, 'The car model is required'],
+			trim: true,
+		},
+
+		color: {
+			type: String,
+			required: [true, 'The car color is required'],
+			trim: true,
+		},
+
+		year: {
+			type: Number,
+			required: [true, 'The car year is required'],
+			min: [1900, 'The car year must be greater than or equal to 1900'],
+			max: [
+				new Date().getFullYear(),
+				'The car year cannot be greater than the current year',
+			],
+		},
+
+		value_per_day: {
+			type: Number,
+			required: [true, 'The car rental value per day is required'],
+			min: [
+				0,
+				'The car rental value per day must be greater than or equal to zero',
+			],
+		},
+
+		accessories: [
+			{
+				description: {
+					type: String,
+					required: [true, 'The accessory description is required'],
+					trim: true,
+				},
+			},
+		],
+
+		number_of_passengers: {
+			type: Number,
+			required: [true, 'The number of passengers is required'],
+			min: [1, 'The number of passengers must be greater than or equal to 1'],
+		},
+	},
+	{ timestamps: true }
+);
+
+const carSchema = model('Car', CarSchema);
+
+export { carSchema };
