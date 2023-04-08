7cbb5f73ff9e6286792947a9d1b0b3ba1829ab73
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 03:28:32 -0400

    refactor: update params on ICarRepository

--- src/modules/car/ICarRepository.ts
+++ src/modules/car/ICarRepository.ts
@@ -5,8 +5,8 @@ interface ICarRepository {
 	registerCar(car: Car): Promise<Car | null>;
 	getAllCars(): Promise<Car[]>;
 	getCarById(carId: string): Promise<Car | null>;
-	getCarByAccessoryId(accessoriesId: string): Promise<Car[]>;
-    getCarByParam(queryParam: string): Promise<Car[]>;
+	getCarByAccessoryId(accessoryId: string): Promise<Car[]>;
+	getCarByQueryParam(attribute: string, value: string): Promise<Car[]>;
 	deleteCarById(carId: string): Promise<Car | null>;
 	updateCar(carId: string, updateParams: unknown): Promise<Car | null>;
 }
