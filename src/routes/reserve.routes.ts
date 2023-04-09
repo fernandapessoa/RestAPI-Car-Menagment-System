83fbc953f5f2a9a6b4d4d59ada8037f76b7ee621
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 22:56:34 -0400

    feat: add auth and reserveController to reserve route

--- src/routes/reserve.routes.ts
+++ src/routes/reserve.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { auth } from '../middlewares/auth';
+import { reserveController } from '../modules/reserve/reserveController';
 
 const reserveRouter = Router();
 
-reserveRouter.post('/'); //register
+reserveRouter.post('/', auth, reserveController.registerReserve); //register
 
 export { reserveRouter };
