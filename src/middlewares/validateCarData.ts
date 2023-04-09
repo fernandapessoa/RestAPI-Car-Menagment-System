ac3e2fec0c136ac223f8c266e20604b9a5a1cab7
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-08 22:48:52 -0400

    feat: validateUpdateCarData middleware

--- src/middlewares/validateCarData.ts
+++ src/middlewares/validateCarData.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
-import { registerCarSchema } from '../validators/carValidators';
+import {
+	registerCarSchema,
+	updateCarSchema,
+} from '../validators/carValidators';
 import { Car } from '../modules/car/ICar';
 import { ValidationError } from 'joi';
 import { AppError } from '../errors/AppError';
@@ -13,6 +16,24 @@ export async function validateCarData(
 		const carData: Car = req.body;
 
 		await registerCarSchema.validateAsync(carData);
+		return next();
+	} catch (err) {
+		if (err instanceof ValidationError) {
+			return next(new AppError(400, err.details[0].message));
+		}
+		return next(err);
+	}
+}
+
+export async function validateUpdateCarData(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const carData = req.body;
+
+		await updateCarSchema.validateAsync(carData);
 		return next();
 	} catch (err) {
 		if (err instanceof ValidationError) {
