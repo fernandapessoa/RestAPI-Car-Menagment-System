a60e75d45d248ed09771ddd51c41a4b186abb348
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 05:36:54 -0400

    refactor: update functions in IReserveRepository

--- src/modules/reserve/IReserveRepository.ts
+++ src/modules/reserve/IReserveRepository.ts
@@ -3,14 +3,17 @@ import { Reserve } from './IReserve';
 
 interface IReserveRepository {
 	registerReserve(reserveData: Reserve): Promise<Reserve | null>;
-	getAllReserves(): Promise<Reserve[] | null>;
+	getAllReserves(userId: string): Promise<Reserve[]>;
 	getReserveById(userId: string, reserveId: string): Promise<Reserve | null>;
 	updateReserveById(
 		userId: string,
 		reserveId: string,
 		updateBody: unknown
 	): Promise<Reserve | null>;
-	deleteReserveById(reserveId: string): Promise<Reserve | null>;
+	getReserveByAttribute(
+		attributes: Record<string, string | number>
+	): Promise<Reserve[]>;
+	deleteReserveById(userId: string, reserveId: string): Promise<Reserve | null>;
 }
 
 export { IReserveRepository };
