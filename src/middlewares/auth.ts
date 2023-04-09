6727f17e807dd96af9ec9e62d35a6adcb3b2fca1
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 09:34:36 -0400

    feat: add carAuth middlewate

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,25 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../security/security';
-//import { AppError } from './../errors/AppError';
+import { AppError } from './../errors/AppError';
 import { userService } from '../modules/user/userService';
 
 export async function auth(req: Request, res: Response, next: NextFunction) {
 	const token = req.headers.authorization?.split(' ')[1];
-
 	if (!token) {
-		return next(new Error('Invalid or expired token'));
+		return next(new AppError(401, 'Bearer token must be provided'));
 	}
 
 	const payload = verifyToken(token);
-
 	if (!payload) {
-		return next(new Error('Invalid or expired token'));
+		return next(new AppError(401, 'Invalid or expired token'));
 	}
 
 	try {
 		req.body.user = await userService.getUser(payload.id);
 	} catch (err) {
 		return next(new Error('Invalid or expired token'));
+	}
+
+	next();
+}
+
+export async function carAuth(req: Request, res: Response, next: NextFunction) {
+	const token = req.headers.authorization?.split(' ')[1];
+	if (!token) {
+		return next(new AppError(401, 'Bearer token must be provided'));
+	}
+
+	const payload = verifyToken(token);
+	if (!payload) {
+		return next(new AppError(401, 'Invalid or expired token'));
 	}
 
 	next();
