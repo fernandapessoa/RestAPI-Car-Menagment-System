9cdfdaa87024d88ff41622df02b3a6f6fc6d7764
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 02:00:40 -0400

    feat: update IReserveRepository

--- src/modules/reserve/IReserveRepository.ts
+++ src/modules/reserve/IReserveRepository.ts
@@ -4,12 +4,13 @@ import { Reserve } from './IReserve';
 interface IReserveRepository {
 	registerReserve(reserveData: Reserve): Promise<Reserve | null>;
 	getAllReserves(): Promise<Reserve[] | null>;
-	getReserveById(reserveId: string): Promise<Reserve[] | null>;
-	getReserveByAttribute(
-		attribute: Record<string, string | number>
-	): Promise<Reserve[]>;
-	updateReserveById(reserveId: string): Promise<Reserve[] | null>;
-	deleteReserveById(reserveId: string): Promise<Reserve[] | null>;
+	getReserveById(userId: string, reserveId: string): Promise<Reserve | null>;
+	updateReserveById(
+		userId: string,
+		reserveId: string,
+		updateBody: unknown
+	): Promise<Reserve | null>;
+	deleteReserveById(reserveId: string): Promise<Reserve | null>;
 }
 
 export { IReserveRepository };
