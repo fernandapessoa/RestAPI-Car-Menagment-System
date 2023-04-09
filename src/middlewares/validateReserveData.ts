6d9cad86216d22daa48492bf6e0906dca825feb4
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 10:35:24 -0400

    feat: add validateReserveData middleware

---
+++ src/middlewares/validateReserveData.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import {
+	registerReserveSchema,
+	updateReserveSchema,
+} from '../validators/reserveValidators';
+import { Reserve } from '../modules/reserve/IReserve';
+import { ValidationError } from 'joi';
+import { AppError } from '../errors/AppError';
+
+export async function validateReserveData(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const reserveData: Reserve = req.body;
+
+		await registerReserveSchema.validateAsync(reserveData);
+		return next();
+	} catch (err) {
+		if (err instanceof ValidationError) {
+			return next(new AppError(400, err.details[0].message));
+		}
+		return next(err);
+	}
+}
+
+export async function validateUpdateReserveData(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const reserveData = req.body;
+
+		await updateReserveSchema.validateAsync(reserveData);
+		return next();
+	} catch (err) {
+		if (err instanceof ValidationError) {
+			return next(new AppError(400, err.details[0].message));
+		}
+		return next(err);
+	}
+}
