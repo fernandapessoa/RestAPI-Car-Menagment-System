cf9aa19fb332dd06898dc542fe7ee05a09b40c6e
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 09:36:31 -0400

    refactor: add pagination params to ICarRepository

--- src/modules/car/ICarRepository.ts
+++ src/modules/car/ICarRepository.ts
@@ -3,10 +3,14 @@ import { Car } from './ICar';
 
 interface ICarRepository {
 	registerCar(carData: Car): Promise<Car | null>;
-	getAllCars(): Promise<Car[]>;
+	getAllCars(skip: number, limit: number): Promise<Car[]>;
 	getCarById(carId: string): Promise<Car | null>;
 	getCarByAccessoryId(accessoryId: string): Promise<Car[]>;
-	getCarByAttribute(attribute: Record<string, string | number>): Promise<Car[]>;
+	getCarByAttribute(
+		attribute: Record<string, string | number>,
+		skip: number,
+		limit: number
+	): Promise<Car[]>;
 	deleteCarById(carId: string): Promise<Car | null>;
 	updateCar(carId: string, updateParams: unknown): Promise<Car | null>;
 }
