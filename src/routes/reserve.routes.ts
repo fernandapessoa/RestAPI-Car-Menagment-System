cc9a9fa268a74445324b4e01a597541d9e0d49de
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 10:38:15 -0400

    feat: add validateReserveData middleware to reserve routes

--- src/routes/reserve.routes.ts
+++ src/routes/reserve.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
 import { auth } from '../middlewares/auth';
 import { reserveController } from '../modules/reserve/reserveController';
+import {
+	validateReserveData,
+	validateUpdateReserveData,
+} from '../middlewares/validateReserveData';
 
 const reserveRouter = Router();
 
-reserveRouter.post('/', auth, reserveController.registerReserve);
-reserveRouter.get('/', auth, reserveController.getReserve);
+reserveRouter.post(
+	'/',
+	auth,
+	validateReserveData,
+	reserveController.registerReserve
+);
+reserveRouter.get(
+	'/',
+	auth,
+	validateUpdateReserveData,
+	reserveController.getReserve
+);
 reserveRouter.get('/:id', auth, reserveController.getReserveById);
 reserveRouter.delete('/:id', auth, reserveController.deleteReserveById);
 reserveRouter.put('/:id', auth, reserveController.updateReserveById);
