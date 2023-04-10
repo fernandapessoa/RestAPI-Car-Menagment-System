37b03f3a8357e3b541ce806954ec06a2c9264df9
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 22:26:51 -0400

    fix: add req.authenticatedUser to userController

--- src/modules/user/userController.ts
+++ src/modules/user/userController.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars*/
 
 import { UserService, userService } from './userService';
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { User } from './IUser';
 import { CatchExpressError } from '../../decorators/CatchExpressError';
+import { AuthenticatedRequest } from '../../type/IRequest';
 
 export class UserController {
 	private userService: UserService;
@@ -13,7 +14,11 @@ export class UserController {
 	}
 
 	@CatchExpressError
-	async registerUser(req: Request, res: Response, _next: NextFunction) {
+	async registerUser(
+		req: AuthenticatedRequest,
+		res: Response,
+		_next: NextFunction
+	) {
 		const userData: User = req.body;
 		const token = await this.userService.registerUser(userData);
 
@@ -26,19 +31,38 @@ export class UserController {
 	}
 
 	@CatchExpressError
-	async getUser(req: Request, res: Response, _next: NextFunction) {
-		const userId = req.body.user._id.toString();
-		const user: User = await this.userService.getUser(userId);
+	async getAllUsers(
+		req: AuthenticatedRequest,
+		res: Response,
+		_next: NextFunction
+	) {
+		const users = await this.userService.getAllUsers();
 
 		return res.status(200).json({
 			status: 'success',
-			data: user,
+			data: users,
+			total: users.length,
 		});
 	}
 
 	@CatchExpressError
-	async deleteUser(req: Request, res: Response, _next: NextFunction) {
-		const userId = req.body.user._id.toString();
+	async getUser(req: AuthenticatedRequest, res: Response, _next: NextFunction) {
+		const userId = req.authenticatedUser._id.toString();
+		const users: User = await this.userService.getUser(userId);
+
+		return res.status(200).json({
+			status: 'success',
+			data: users,
+		});
+	}
+
+	@CatchExpressError
+	async deleteUser(
+		req: AuthenticatedRequest,
+		res: Response,
+		_next: NextFunction
+	) {
+		const userId = req.authenticatedUser._id.toString();
 		await this.userService.deleteUser(userId);
 
 		return res.status(204).json({
@@ -48,8 +72,12 @@ export class UserController {
 	}
 
 	@CatchExpressError
-	async updateUser(req: Request, res: Response, _next: NextFunction) {
-		const userId = req.body.user._id.toString();
+	async updateUser(
+		req: AuthenticatedRequest,
+		res: Response,
+		_next: NextFunction
+	) {
+		const userId = req.authenticatedUser._id.toString();
 		const updateBody = req.body;
 		const user: User = await this.userService.updateUser(userId, updateBody);
 
@@ -60,7 +88,11 @@ export class UserController {
 	}
 
 	@CatchExpressError
-	async authenticateUser(req: Request, res: Response, _next: NextFunction) {
+	async authenticateUser(
+		req: AuthenticatedRequest,
+		res: Response,
+		_next: NextFunction
+	) {
 		const { email, password } = req.body;
 		const token = await this.userService.authenticateUser(email, password);
 
