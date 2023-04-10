d3f35ed83709ee90eaba108afa8e79e58a31840d
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 22:16:41 -0400

    feat: add reserveAuth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,8 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../security/security';
 import { AppError } from './../errors/AppError';
 import { userService } from '../modules/user/userService';
+import { AuthenticatedRequest } from '../type/IRequest';
 
-export async function auth(req: Request, res: Response, next: NextFunction) {
+export async function auth(
+	req: AuthenticatedRequest,
+	res: Response,
+	next: NextFunction
+) {
 	const token = req.headers.authorization?.split(' ')[1];
 	if (!token) {
 		return next(new AppError(401, 'Bearer token must be provided'));
@@ -15,7 +20,7 @@ export async function auth(req: Request, res: Response, next: NextFunction) {
 	}
 
 	try {
-		req.body.user = await userService.getUser(payload.id);
+		req.authenticatedUser = await userService.getUser(payload.id);
 	} catch (err) {
 		return next(new Error('Invalid or expired token'));
 	}
@@ -32,6 +37,30 @@ export async function carAuth(req: Request, res: Response, next: NextFunction) {
 	const payload = verifyToken(token);
 	if (!payload) {
 		return next(new AppError(401, 'Invalid or expired token'));
+	}
+
+	next();
+}
+
+export async function reserveAuth(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const token = req.headers.authorization?.split(' ')[1];
+	if (!token) {
+		return next(new AppError(401, 'Bearer token must be provided'));
+	}
+
+	const payload = verifyToken(token);
+	if (!payload) {
+		return next(new AppError(401, 'Invalid or expired token'));
+	}
+
+	try {
+		req.body.user = await userService.getUser(payload.id);
+	} catch (err) {
+		return next(new Error('Invalid or expired token'));
 	}
 
 	next();
