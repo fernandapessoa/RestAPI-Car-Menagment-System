b4e35711b06d256d2e12f5e4bfbb00988ee3553a
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-04-09 22:25:52 -0400

    feat: add getAllUsers to IUserRepository

--- src/modules/user/IUserRepository.ts
+++ src/modules/user/IUserRepository.ts
@@ -4,6 +4,7 @@ import { User } from './IUser';
 interface IUserRepository {
 	registerUser(user: User): Promise<User | null>;
 	getUser(userId: string): Promise<User | null>;
+	getAllUsers(): Promise<User[] | null>;
 	deleteUser(userId: string): Promise<User | null>;
 	updateUser(userId: string, updateParams: unknown): Promise<User | null>;
 	findUserByEmail(email: string): Promise<User | null>;
